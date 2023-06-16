7bdc90d6cc637370630f482510339350bf4081f0
Shahin Nezam <shahinnezam@hotmail.com>
2023-06-16 14:14:13 -0700

    Added persistentEnum and priorityLevel var and switch cases

--- App/Item.swift
+++ App/Item.swift
@@ -5,4 +5,21 @@ class Item: Object, ObjectKeyIdentifiable {
     @Persisted var isComplete = false
     @Persisted var summary: String
     @Persisted var owner_id: String
+    @Persisted var priority: PriorityLevel
+}
+
+enum PriorityLevel: Int, PersistableEnum, CaseIterable {
+    case severe = 0
+    case high = 1
+    case medium = 2
+    case low = 3
+    
+    var description: String {
+        switch self {
+            case .severe: return "Severe"
+            case .high: return "High"
+            case .medium: return "Medium"
+            case .low: return "Low"
+        }
+    }
 }
